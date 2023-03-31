c3fe2711864261e5c31ba6e891c21e9aef271a23
Rashida ios <markwalsh6809@gmail.com>
2023-04-01 06:21:48 +0900

    Delete 桌面快捷方式.bat

--- bat/桌面快捷方式.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem exe=需要创建的文件(一般以exe结尾) link=快捷方式名称 ico会自动读取需要创建的文件
-set "exe=xxx.txt"
-set "lnk=xxx
-mshta VBScript:Execute("Set a=CreateObject(""WScript.Shell""):Set b=a.CreateShortcut(a.SpecialFolders(""Desktop"") & ""\%lnk%.lnk""):b.TargetPath=""%~dp0%exe%"":b.WorkingDirectory=""%~dp0"":b.Save:close")
-echo 桌面快捷方式已创建完成&pause 
-rem 如果需要执行完不关闭cmd窗口，请取消 echo 桌面快捷方式已创建完成&pause 前的注释
